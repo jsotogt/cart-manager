9b9cb673c94715053da1385b6d524cd383dcc2fc
Jaime Soto <jsoto.gt@gmail.com>
2016-07-01 11:06:56 -0400

    Initial commit

---
+++ app/models/Quote.java
@@ -0,0 +1,17 @@
+package models;
+
+/**
+ * Sample json object.
+ */
+public class Quote {
+
+    private final String message;
+
+    public Quote(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
